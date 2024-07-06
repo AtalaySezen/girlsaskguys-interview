70dcf5c11592d7fff6a75d84d12e4eae4973a468
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-07-07 02:58:40 +0300

    feats : mobile navbar added. changes : mobile responsive developments completed.

---
+++ js/home.js
@@ -0,0 +1,103 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bodyOverlay = document.getElementById("popup-overlay");
+  const popupContainer = document.getElementById("popup-container");
+  const popupFirstContent = document.getElementById("popup-first-content");
+  const popupSecondContent = document.getElementById("popup-second-content");
+  const popupThirdContent = document.getElementById("popup-third-content");
+  const popupHeader = document.getElementById("popup-header");
+  const planNowButton = document.getElementById("planNowButton");
+  const planNowLoaderButton = document.querySelector(
+    ".popup-footer #planNowButton"
+  );
+  const closeButton = document.getElementById("close-btn");
+  const exploreNowButton = document.getElementById("btn-explore");
+  const inputDateSelector = document.getElementById("trip-date");
+  const planVacationImage = document.getElementById("plan-vacation");
+  let timeOut;
+
+  const checkPastDates = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, "0");
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
+    const yyyy = today.getFullYear();
+    const formattedDate = `${yyyy}-${mm}-${dd}`;
+    inputDateSelector.min = formattedDate;
+  };
+
+  const startPlanVacation = () => {
+    bodyOverlay.style.display = "block";
+    popupContainer.style.display = "flex";
+  };
+
+  const closePopup = () => {
+    bodyOverlay.style.display = "none";
+    popupContainer.style.display = "none";
+    popupFirstContent.style.display = "flex";
+    popupSecondContent.style.display = "none";
+    planNowButton.style.display = "flex";
+    planNowLoaderButton.style.display = "flex";
+    popupHeader.innerHTML = "Plan Your Vacation";
+    popupThirdContent.style.display = "none";
+    clearVacationTimeout();
+  };
+
+  const validateForm = () => {
+    const inputs = document.querySelectorAll(".popup-input");
+    let isValid = true;
+    inputs.forEach((input) => {
+      const errorElement = input.nextElementSibling;
+      if (input.value.trim() === "") {
+        errorElement.textContent = `Please fill out this field.`;
+        errorElement.style.display = "block";
+        isValid = false;
+      } else {
+        errorElement.style.display = "none";
+      }
+    });
+    return isValid;
+  };
+
+  const planVacation = () => {
+    popupFirstContent.style.display = "none";
+    popupSecondContent.style.display = "flex";
+    timeOut = setTimeout(() => {
+      popupSecondContent.style.display = "none";
+      planNowButton.style.display = "none";
+      planNowLoaderButton.style.display = "none";
+      popupHeader.innerHTML = "Travel Guide";
+      popupThirdContent.style.display = "flex";
+      clearVacationTimeout();
+    }, 2000);
+  };
+
+  const clearVacationTimeout = () => {
+    clearTimeout(timeOut);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      planVacation();
+    }
+  };
+
+  const handleEscapeKey = (event) => {
+    if (event.key === "Escape") {
+      closePopup();
+    }
+  };
+
+  checkPastDates();
+
+  closeButton.addEventListener("click", closePopup);
+  exploreNowButton.addEventListener("click", closePopup);
+  planVacationImage.addEventListener("click", startPlanVacation);
+  popupFirstContent.addEventListener("submit", handleFormSubmit);
+  document.addEventListener("keydown", handleEscapeKey);
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      closePopup();
+    }
+  });
+});
--- js/main.js
+++ js/main.js
@@ -1,103 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const popupOverlay = document.getElementById("popup-overlay");
-  const popupContainer = document.getElementById("popup-container");
-  const popupFirstContent = document.getElementById("popup-first-content");
-  const popupSecondContent = document.getElementById("popup-second-content");
-  const popupThirdContent = document.getElementById("popup-third-content");
-  const popupHeader = document.getElementById("popup-header");
-  const planNowButton = document.getElementById("planNowButton");
-  const planNowLoaderButton = document.querySelector(
-    ".popup-footer #planNowButton"
-  );
-  const closeButton = document.getElementById("close-btn");
-  const exploreNowButton = document.getElementById("btn-explore");
-  const inputDateSelector = document.getElementById("trip-date");
-  const planVacationImage = document.getElementById("plan-vacation");
-  let timeOut;
+  const mobileMenuButton = document.getElementById("mobile-menu-icon");
+  const mobileMenuIcon = document.getElementById("mobile-menu-icon");
+  const mobileSideBar = document.getElementById("mobile-menu");
+  const mobileHeaderItems = document.querySelectorAll(".mobile-header-item");
+  const bodyOverlay = document.getElementById("popup-overlay");
 
-  const checkPastDates = () => {
-    const today = new Date();
-    const dd = String(today.getDate()).padStart(2, "0");
-    const mm = String(today.getMonth() + 1).padStart(2, "0");
-    const yyyy = today.getFullYear();
-    const formattedDate = `${yyyy}-${mm}-${dd}`;
-    inputDateSelector.min = formattedDate;
+  const openSidenav = () => {
+    mobileSideBar.classList.toggle("mobile-active");
+    checkBodyOverlay();
   };
 
-  const startPlanVacation = () => {
-    popupOverlay.style.display = "block";
-    popupContainer.style.display = "flex";
+  const closeSidenav = () => {
+    mobileSideBar.classList.remove("mobile-active");
+    checkBodyOverlay();
   };
 
-  const closePopup = () => {
-    popupOverlay.style.display = "none";
-    popupContainer.style.display = "none";
-    popupFirstContent.style.display = "flex";
-    popupSecondContent.style.display = "none";
-    planNowButton.style.display = "flex";
-    planNowLoaderButton.style.display = "flex";
-    popupHeader.innerHTML = "Plan Your Vacation";
-    popupThirdContent.style.display = "none";
-    clearVacationTimeout();
+  const checkBodyOverlay = () => {
+    if (mobileSideBar.classList.contains("mobile-active")) {
+      bodyOverlay.style.display = "block";
+    } else {
+      bodyOverlay.style.display = "none";
+    }
   };
 
-  const validateForm = () => {
-    const inputs = document.querySelectorAll(".popup-input");
-    let isValid = true;
-    inputs.forEach((input) => {
-      const errorElement = input.nextElementSibling;
-      if (input.value.trim() === "") {
-        errorElement.textContent = `Please fill out this field.`;
-        errorElement.style.display = "block";
-        isValid = false;
-      } else {
-        errorElement.style.display = "none";
+  mobileMenuButton.addEventListener("click", openSidenav);
+
+  document.body.addEventListener("click", (event) => {
+    const target = event.target;
+    const isMobileHeaderItem = Array.from(mobileHeaderItems).some((item) =>
+      item.contains(target)
+    );
+
+    const isMobileSidebar = mobileSideBar.contains(target);
+    const checkMenuButton = mobileMenuButton.contains(target);
+    const checkMenuIcon = mobileMenuIcon.contains(target);
+
+    if (mobileSideBar.classList.contains("mobile-active")) {
+      if (
+        !isMobileHeaderItem &&
+        !isMobileSidebar &&
+        !checkMenuButton &&
+        !checkMenuIcon
+      ) {
+        closeSidenav();
       }
-    });
-    return isValid;
-  };
-
-  const planVacation = () => {
-    popupFirstContent.style.display = "none";
-    popupSecondContent.style.display = "flex";
-    timeOut = setTimeout(() => {
-      popupSecondContent.style.display = "none";
-      planNowButton.style.display = "none";
-      planNowLoaderButton.style.display = "none";
-      popupHeader.innerHTML = "Travel Guide";
-      popupThirdContent.style.display = "flex";
-      clearVacationTimeout();
-    }, 2000);
-  };
-
-  const clearVacationTimeout = () => {
-    clearTimeout(timeOut);
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    if (validateForm()) {
-      planVacation();
-    }
-  };
-
-  const handleEscapeKey = (event) => {
-    if (event.key === "Escape") {
-      closePopup();
-    }
-  };
-
-  checkPastDates();
-
-  closeButton.addEventListener("click", closePopup);
-  exploreNowButton.addEventListener("click", closePopup);
-  planVacationImage.addEventListener("click", startPlanVacation);
-  popupFirstContent.addEventListener("submit", handleFormSubmit);
-  document.addEventListener("keydown", handleEscapeKey);
-
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-      closePopup();
     }
   });
 });
